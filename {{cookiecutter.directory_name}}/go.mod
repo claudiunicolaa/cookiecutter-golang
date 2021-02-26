c217cade891539cb21898546c2882c6b8b3b9051
claudiunicolaa <claudiunicola96@gmail.com>
2021-02-26 17:38:11 +0200

    Add main.go file. Set go version 1.16

--- {{cookiecutter.directory_name}}/go.mod
+++ {{cookiecutter.directory_name}}/go.mod
@@ -1,3 +1,3 @@
 module github.com/{{cookiecutter.github_username}}/{{cookiecutter.directory_name}}
 
-go 1.15
+go 1.16
---
+++ {{cookiecutter.directory_name}}/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hi from github.com/{{cookiecutter.github_username}}/{{cookiecutter.directory_name}}")
+}
+
